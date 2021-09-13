1dd93ede8705bbd443167870984ea93b49a044ea
Albin <albin.nojback@gmail.com>
2021-09-13 08:44:53 +0200

    Initial Commit

---
+++ Tamagothci/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tamagothci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tamagotchi pet = new Tamagotchi();
+
+
+            string playerInput;
+
+            Console.WriteLine("What is the pets name?");
+
+            pet.name = Console.ReadLine();
+
+            while (pet.GetAlive())
+            {
+                Console.WriteLine($"What do you want to do with {pet.name}?4");
+                Console.WriteLine("1-Feed, 2-Greet, 3-Teach, 4-stats");
+                playerInput = Console.ReadLine();
+
+                while (!PlayerChoice(playerInput, pet))
+                {
+                    Console.WriteLine("Invalid Input");
+                    playerInput = Console.ReadLine();
+                }
+
+                pet.Tick();
+
+            }
+            Console.WriteLine("You Fail");
+            Console.ReadLine();
+
+        }
+
+        static bool PlayerChoice(string input, Tamagotchi p)
+        {
+            switch (input)
+            {
+                case "1":
+                    p.Feed();
+                    return true;
+
+                case "2":
+                    p.Hi();
+                    return true;
+
+                case "3":
+                    Console.WriteLine("What do you want to teach?");
+                    p.Teach(Console.ReadLine());
+                    return true;
+
+                case "4":
+                    p.PrintStats();
+                    return true;
+
+                default:
+                    return false;
+            }
+
+        }
+    }
+}
---
+++ Tamagothci/Tamagotchi.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tamagothci
+{
+    public class Tamagotchi
+    {
+        private int hunger = 0;
+        private int boredom = 0;
+        private List<string> words = new List<string>();
+        private bool isAlive = true;
+        private Random r = new();
+        public string name;
+
+        public void Feed()
+        {
+            hunger--;
+        }
+
+        public void Hi()
+        {
+            Console.WriteLine(words[r.Next(words.Count)]);
+            ReduceBoredom();
+        }
+
+        public void Teach(string word)
+        {
+            words.Add(word);
+            ReduceBoredom();
+
+        }
+
+        public void Tick()
+        {
+            hunger++;
+            boredom++;
+
+            if (hunger > 10 || boredom > 10)
+            {
+                isAlive = false;
+            }
+        }
+        public void PrintStats()
+        {
+
+            if (isAlive == true)
+            {
+                Console.WriteLine($"Your Pet {name} has {hunger} Hunger and {boredom} Boredom!");
+            }
+            else
+            {
+                Console.WriteLine($"Your Pet {name} is DEAD");
+            }
+        }
+
+        public bool GetAlive()
+        {
+            return isAlive;
+        }
+
+        private void ReduceBoredom()
+        {
+            boredom--;
+        }
+    }
+}
